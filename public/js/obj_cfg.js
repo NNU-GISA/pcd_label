dc17b6b78d458463959710e24f79776a2cd96d90
Naurril <naurril@gmail.com>
2019-10-31 14:40:21 +0800

    color of objects adjusted

--- public/js/obj_cfg.js
+++ public/js/obj_cfg.js
@@ -1,16 +1,14 @@
-
+// size is the dimension of the object in x/y/z axis.
 var obj_type_map = {
-    Car: {color: '#00ff00',  size:[1.8, 4.5, 1.5]},
-    Bus: {color: '#ffff00',  size:[2.8, 10, 3]},
+    Car:        {color: '#00ff00',  size:[1.8, 4.5, 1.5]},
+    Bus:        {color: '#ffff00',  size:[2.8, 10, 3]},
     Pedestrian: {color: '#ff0000',  size:[0.5, 0.4, 1.7]},
-    Rider: {color: '#ff88f0',  size:[0.6, 1.6, 1.6]},
-    Bicyclist: {color: '#ff8800',  size:[0.6, 1.6, 1.2]},
-    Bicycle: {color: '#ff8800',  size:[0.6, 1.6, 1.2]},
-    Motorcyclist: {color: '#ff8800',  size:[0.6, 1.6, 1.2]},
-    Motor: {color: '#ff8800',  size:[0.6, 1.6, 1.2]},
-    Truck: {color: '#ffff00',  size:[2.8, 10, 3]},
-    Animal: {color: '#ff0088',  size:[0.6, 1.6, 1.2]},
-    Unknown: {color: '#ff00ff',  size:[1.8, 4.5, 1.5]},
+    Rider:      {color: '#ff8800',  size:[0.6, 1.6, 1.6]},
+    Bicycle:    {color: '#88ff00',  size:[0.6, 1.6, 1.2]},
+    Motor:      {color: '#aaaa00',  size:[0.6, 1.6, 1.2]},
+    Truck:      {color: '#00ffff',  size:[2.8, 10, 3]},
+    Animal:     {color: '#0000ff',  size:[0.6, 1.6, 1.2]},
+    Unknown:    {color: '#008888',  size:[1.8, 4.5, 1.5]},
 }
 
 export {obj_type_map}
